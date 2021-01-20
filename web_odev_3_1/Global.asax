6843bcff200925ed1e3a6998fc278f0440731f49
Sena Kocak <56205378+Sena4514@users.noreply.github.com>
2021-01-21 01:26:17 +0300

    Calculator(with Jquery)
    I made a calculator with using html, css and jquery.

---
+++ web_odev_3_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="web_odev_3_1.Global_asax" Language="vb" %>
---
+++ web_odev_3_1/myscript.js
@@ -0,0 +1,13 @@
+﻿function ekle(sayi) {
+    $(".textview").val($(".textview").val() + sayi);
+}
+function esittir() {
+    $(".textview").val(eval($(".textview").val()));
+}
+function temizle() {
+    $(".textview").val('');
+}
+function geriAl() {
+    deger = $(".textview").val();
+    $(".textview").val(deger.substring(0, deger.length - 1));
+}
